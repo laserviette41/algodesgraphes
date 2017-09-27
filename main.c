2c73d233a04b4d393b62ca2d4766dbb8dedc99c2
Gabriel M <31955151+laserviette41@users.noreply.github.com>
2017-09-27 09:48:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(){
 	ptr_list *p=malloc(sizeof(ptr_list));
 	p->pr=NULL;
 	p->der=NULL;
-    int prm[2];
+    int prm[2]; // prm[0] nb clause prm[1] nb variable
 	parseur("fichier.cnf",p);
 	affiche_formule(*p);
 	libere(p);
